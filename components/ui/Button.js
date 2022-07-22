681a60fddb3dac60ff6b70563d668da9935d046e
Thien Trinh <thien.trinh@hcl.com>
2022-07-22 18:42:00 +0700

    Add event search component and style

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -3,10 +3,18 @@ import Link from 'next/link';
 
 import { btn } from './button.module.css';
 
-export default function Button({ link, children }) {
+export default function Button({ link, children, onCLick }) {
+  if (link) {
+    return (
+      <Link href={link}>
+        <a className={btn}>{children}</a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={link}>
-      <a className={btn}>{children}</a>
-    </Link>
+    <button className={btn} onClick={onCLick}>
+      {children}
+    </button>
   );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import EventList from '../../components/event/EventList';
+import EventSearch from '../../components/event/EventSearch';
 import { getAllEvents } from '../../dummy-data';
 
 export default function EventsPage() {
   const events = getAllEvents();
 
   return (
-    <div>
+    <>
+      <EventSearch />
       <EventList item={events} />
-    </div>
+    </>
   );
 }
