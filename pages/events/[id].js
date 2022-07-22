c9765fd80f8391859f1fad820a13679556423d20
Thien Trinh <thien.trinh@hcl.com>
2022-07-22 16:48:33 +0700

    Add event detail component to dynamic route page

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useRouter } from 'next/router';
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventContent from '../../components/event-detail/event-content';
 
 export default function EventDetailPage() {
+  const router = useRouter();
+
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  const { title, description, location, date, image } = event;
+
   return (
-    <div>EventDetailPage</div>
-  )
+    <>
+      <EventSummary title={title} />
+      <EventLogistics
+        date={date}
+        address={location}
+        image={image}
+        imageAlt={title}
+      />
+      <EventContent>
+        <p>{description}</p>
+      </EventContent>
+    </>
+  );
 }
