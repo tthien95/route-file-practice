632e8bc4dbdbd7db6efbe36b9ec0abe3a2bc472e
Thien Trinh <thien.trinh@hcl.com>
2022-07-29 10:55:33 +0700

    Minor fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,6 @@ import '../styles/globals.css';
 import Layout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
-  if (process.env.NODE_ENV == 'development') {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
-  }
-
   return (
     <Layout>
       <Component {...pageProps} />
