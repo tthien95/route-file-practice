c55be844638d389ed9c5eb58dfa02dc902456bca
Thien Trinh <thien.trinh@hcl.com>
2022-08-01 15:05:44 +0700

    Example of using Next's Image

--- components/event/EventItem.js
+++ components/event/EventItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './event-item.module.css';
 import Button from '../ui/Button';
 import DateIcon from '../icons/date-icon';
@@ -17,7 +18,7 @@ export default function EventItem({ title, image, date, location, id }) {
 
   return (
     <li className={styles.item}>
-      <img src={'/' + image} alt={title} />
+      <Image src={'/' + image} alt={title} width={250} height={160} />
       <div className={styles.content}>
         <div>
           <h2>{title}</h2>
