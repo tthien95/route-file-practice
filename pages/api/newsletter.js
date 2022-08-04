2b0912098490636b9502527dc2328f1b6b988339
Thien Trinh <thien.trinh@hcl.com>
2022-08-04 12:03:06 +0700

    Connect to mongo to store newsletter email

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email } = req.body;
 
@@ -7,7 +9,16 @@ function handler(req, res) {
       return;
     }
 
-    console.log(email);
+    const client = await MongoClient.connect(
+      'mongodb+srv://<username>:<password>@cluster0.xb7hz.mongodb.net/?retryWrites=true&w=majority'
+    );
+
+    const db = client.db();
+
+    await db.collection('emails').insertOne({ email: email });
+
+    client.close();
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
